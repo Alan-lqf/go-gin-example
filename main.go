96340fc71abf5826df9c378fb92b3cb373d91972
alan <1993826783@qq.com>
2022-07-18 21:40:52 +0800

    test1

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-    "blog-service/internal/routers"
-    "net/http"
-    "time"
+	"blog-service/internal/routers"
+	"net/http"
+	"time"
 )
 
 func main() {
-    router := routers.NewRouter()
-    s := &http.Server{
-        Addr:              ":8080",
-        Handler:           router,
-        TLSConfig:         nil,
-        ReadTimeout:       10 * time.Second,
-        ReadHeaderTimeout: 0,
-        WriteTimeout:      10 * time.Second,
-        IdleTimeout:       0,
-        MaxHeaderBytes:    1 << 20,
-        TLSNextProto:      nil,
-        ConnState:         nil,
-        ErrorLog:          nil,
-        BaseContext:       nil,
-        ConnContext:       nil,
-    }
-    s.ListenAndServe()
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:              ":8080",
+		Handler:           router,
+		TLSConfig:         nil,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 0,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       0,
+		MaxHeaderBytes:    1 << 20,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+	s.ListenAndServe()
 }
+
+// test git grep test
